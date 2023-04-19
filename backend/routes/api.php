2f62b86ca32b8ba54b13765443f233ecd3275eae
David Rallo <davidrallo28@gmail.com>
2023-04-19 15:24:24 -0400

    Update api.php

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -17,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/ext/setUser', [PageController::class, 'setUser']);
+Route::get('/ext/getUsers', [PageController::class, 'getUsers']);
