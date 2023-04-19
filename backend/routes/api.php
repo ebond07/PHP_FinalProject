9d24dcc0d67e750bb433714cace1e303cdce556e
David Rallo <davidrallo28@gmail.com>
2023-04-19 16:04:42 -0400

    Thinhgs

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PageController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,5 +19,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('/ext/setUser', [PageController::class, 'setUser']);
-Route::get('/ext/getUsers', [PageController::class, 'getUsers']);
+Route::post('/api/setUser', [PageController::class, 'setUser']);
+Route::get('/api/getUsers', [PageController::class, 'getUsers']);
