0f5c57a4e7196d71ff1f584aa6866c46dbe9ce37
Adambon1 <119423156+Adambon1@users.noreply.github.com>
2023-04-24 16:31:28 -0400

    register

--- frontend/HTML/JS/register.js
+++ frontend/HTML/JS/register.js
@@ -1,16 +1,17 @@
 // script.js
 
-const form = document.getElementById('register-form');
+const form = document.querySelector('#register-form');
 
 form.addEventListener('submit', async (event) => {
   event.preventDefault(); // prevent the default form submission
 
-  const name = document.getElementById('name').value;
-  const email = document.getElementById('email').value;
-  const password = document.getElementById('password').value;
+const name = document.querySelector('#name').value;
+const email = document.querySelector('#email').value;
+const password = document.querySelector('#password').value;
+
 
   try {
-    const response = await fetch('/v1/users', {
+    const response = await fetch('http://127.0.0.1:8080/api/v1/users', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
@@ -20,8 +21,9 @@ form.addEventListener('submit', async (event) => {
 
     if (response.ok) {
       alert('User registration successful!');
-      window.location.href = 'login.html'; // redirect to login page
+      window.location.href = '/frontend/HTML/login.html'; // redirect to login page
     } else {
+        console.log(response.status)
       throw new Error('User registration failed');
     }
   } catch (error) {
